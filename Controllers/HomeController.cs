197468a53cf19ea9a3f8a2ad0ac284ec48e07138
TuanDZ <cry.dtuan1608@gmail.com>
2024-11-12 11:38:49 +0700

    12/11/2024 11:40

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTPMUD.Controllers
+{
+    internal class BaseController : System.Mvc.Controller
+    {
+        public virtual object Index() => View();
+    }
+
+    internal class HomeController : BaseController
+    {
+        public object Login() => View();
+    }
+}
---
+++ Views/BaseView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceConfig.Views
+{
+    public class BaseView<TControl, TModel> : System.Mvc.IView
+        where TControl : System.Windows.UIElement, new()
+    {
+        public TControl MainContent { get; set; }
+        public TModel Model { get; set; }
+        public object Content => MainContent;
+
+        protected virtual void RenderCore() { }
+        public void Render(object model)
+        {
+            Model = (TModel)model;
+            MainContent = new TControl();
+
+            RenderCore();
+        }
+    }
+}
